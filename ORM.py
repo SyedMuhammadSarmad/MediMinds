2e0357ab6e8bcb2a19903c3aa5327fefa50e56b0
Syed Muhammad Sarmad <smsarmad92@gmail.com>
2025-07-02 13:00:18 +0500

    database and ORM created

---
+++ ORM.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Text
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import create_engine
+import os 
+
+from dotenv import load_dotenv
+load_dotenv()
+
+Base = declarative_base()
+
+class Patient(Base):
+    __tablename__ = 'patients'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    reports = relationship("MedicalReport", back_populates="patient")
+
+class MedicalReport(Base):
+    __tablename__ = 'medical_reports'
+    id = Column(Integer, primary_key=True)
+    patient_id = Column(Integer, ForeignKey('patients.id'))
+    content = Column(Text)
+    patient = relationship("Patient", back_populates="reports")
+
+
+db_uri = os.environ['DATABASE_URL'].replace("postgresql://", "cockroachdb://")
+
+here = os.path.dirname(__file__)
+cert_path = os.path.join(here, ".cert", "root.crt")
+engine = create_engine(
+
+    db_uri, 
+    
+    connect_args={
+    "sslmode": "verify-full",
+    "sslrootcert": cert_path,
+    "application_name": "mediminds",
+}
+
+)
+try:
+    Base.metadata.create_all(engine)
+    print("Database connected")
+except Exception as e:
+    print("Failed to connect to database.")
+    print(f"{e}")
+
+
